f21f08e2ee9faa33a3b780abf595c5f7f966e497
Sangyeop-Lim <dladla5675@gmail.com>
2025-05-27 17:40:12 +0900

    feat : 코루틴 강의
    코루틴 강의

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField]
+    private Button bounceButton;
+    private Animator ballAnimator = null;
+
+    IEnumerator Start()
+    {
+        ballAnimator = GetComponent<Animator>();
+        yield return new WaitUntil(() => ballAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle"));
+        bounceButton.interactable = true;
+    }
+
+    public void OnClickButton()
+    {
+        StartCoroutine(DoBounce());
+    }
+
+    private IEnumerator DoBounce()
+    {
+        bounceButton.interactable = false;
+        ballAnimator.Play("Bounce");
+
+        yield return new WaitForEndOfFrame();
+
+        yield return new WaitUntil(() => ballAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle"));
+
+        bounceButton.interactable = true;
+    }
+}
